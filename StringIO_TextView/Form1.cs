8b526311871d7ea8870edcd79787adfacda8a687
Heekyoung Park <acdong0202@gmail.com>
2019-05-07 15:57:36 +0900

    #2 사용자 입력을 버튼을 누르면 라벨이 출력

--- StringIO_TextView/Form1.cs
+++ StringIO_TextView/Form1.cs
@@ -7,18 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace StringIO_TextView
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace StringIO_TextView{
+    public partial class Form1 : Form {
+        string OrgStr = "";
+        public Form1() {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+        private void Form1_Load(object sender, EventArgs e) {
+            OrgStr = this.lblResult.Text;
         }
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.txtEdit.Text;
+        }
+
+
     }
 }
